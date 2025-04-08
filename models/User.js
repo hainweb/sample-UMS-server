2a01b1747c1fd9e8e8990b2a6d36f2b62ca521b8
hain <hainofficialweb@gmail.com>
2025-04-08 18:09:29 +0530

    refactor(user): User schema updated

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userScheme = new mongoose.Schema({
   age: { type: Number, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  role: { type: String, required: true },
   createdAt: { type: Date, required: true },
 });
 
